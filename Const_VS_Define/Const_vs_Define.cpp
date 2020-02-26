a0c44573965e2c7821eed6a05feed1cb8b3405b3
ijm <ivan.jaraiz@gmail.com>
2020-02-26 18:20:03 +0100

    diferencia de definición de const y define

---
+++ Const_VS_Define/Const_vs_Define.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.1416; // aquí se declara si es un define
+
+int main(){
+	const float PI = 3.1416; // aquí definimos una cosntate llamada PI
+     	
+	return 0;
+}
